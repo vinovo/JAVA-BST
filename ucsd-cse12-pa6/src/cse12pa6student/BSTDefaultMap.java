a24c32648064c8af2e38bdbcf9676979948bce34
vino <vino@128.54.175.116>
2018-02-18 20:33:36 -0800

    2018-2-18 20:33:20 updated
    bugs fixed, tests added.

--- ucsd-cse12-pa6/src/cse12pa6student/BSTDefaultMap.java
+++ ucsd-cse12-pa6/src/cse12pa6student/BSTDefaultMap.java
@@ -28,12 +28,13 @@ public class BSTDefaultMap<K extends Comparable<K>, V> implements DefaultMap<K, 
 			this.root = new Node<K, V>(key, value);
 		else {
 			Node<K, V> node = root.findPos(key);
-			if (node == null)
-				node = new Node<K, V>(key, value);
-			else {
+			if (key.compareTo(node.k) == 0) {
 				node.v = value;
-				size--; // If the key is already in the tree, no need to
-						// increment size.
+				size--;
+			} else if (key.compareTo(node.k) < 0) {
+				node.left = new Node<K, V>(key, value);
+			} else {
+				node.right = new Node<K, V>(key, value);
 			}
 		}
 		size++;
@@ -42,11 +43,15 @@ public class BSTDefaultMap<K extends Comparable<K>, V> implements DefaultMap<K, 
 	@Override
 	public V get(K key) {
 		V toReturn = null;
-		Node<K, V> node = root.findPos(key);
-		if (node == null)
+		if (size == 0) {
 			toReturn = this.defaultValue;
-		else
-			toReturn = node.v;
+		} else {
+			Node<K, V> node = root.findPos(key);
+			if (key.compareTo(node.k) != 0)
+				toReturn = this.defaultValue;
+			else
+				toReturn = node.v;
+		}
 		if (toReturn == null)
 			throw new NoSuchElementException();
 		else
@@ -55,11 +60,13 @@ public class BSTDefaultMap<K extends Comparable<K>, V> implements DefaultMap<K, 
 
 	@Override
 	public boolean containsKey(K key) {
-		Node<K, V> node = root.findPos(key);
-		if (node == null)
+		if (size == 0)
 			return false;
-		else
+		Node<K, V> node = root.findPos(key);
+		if (key.compareTo(node.k) == 0)
 			return true;
+		else
+			return false;
 	}
 
 	@Override
@@ -75,6 +82,8 @@ public class BSTDefaultMap<K extends Comparable<K>, V> implements DefaultMap<K, 
 	@Override
 	public List<K> keys() {
 		List<K> keyList = new ArrayList<K>();
+		if (size == 0)
+			return keyList;
 		List<Node<K, V>> nodeList = root.formList();
 		for (int i = 0; i < nodeList.size(); i++) {
 			keyList.add(nodeList.get(i).k);
@@ -85,6 +94,8 @@ public class BSTDefaultMap<K extends Comparable<K>, V> implements DefaultMap<K, 
 	@Override
 	public List<V> values() {
 		List<V> valueList = new ArrayList<V>();
+		if (size == 0)
+			return valueList;
 		List<Node<K, V>> nodeList = root.formList();
 		for (int i = 0; i < nodeList.size(); i++) {
 			valueList.add(nodeList.get(i).v);
@@ -106,17 +117,19 @@ public class BSTDefaultMap<K extends Comparable<K>, V> implements DefaultMap<K, 
 			right = null;
 		}
 
+		// this method find and return the last existing node the key can be
+		// compared to.
 		public Node<key, value> findPos(key K) {
 			if (K.compareTo(this.k) == 0)
 				return this;
 			else if (K.compareTo(this.k) < 0) {
 				if (this.left == null)
-					return this.left;
+					return this;
 				else
 					return this.left.findPos(K);
 			} else {
 				if (this.right == null)
-					return this.right;
+					return this;
 				else
 					return this.right.findPos(K);
 			}
@@ -135,5 +148,17 @@ public class BSTDefaultMap<K extends Comparable<K>, V> implements DefaultMap<K, 
 			}
 			return lst;
 		}
+	}
+
+	public static void main(String[] args) {
+		BSTDefaultMap<String, Integer> b = new BSTDefaultMap<String, Integer>(0);
+		b.set("", 15);
+		b.set("A", 20);
+		String a = "";
+		String c = "A";
+		System.out.println(c.compareTo(a));
+		System.out.println(b.root.right != null);
+		System.out.println(b.size);
+		System.out.println((Integer) b.get("A"));
 	}
 }
--- ucsd-cse12-pa6/src/cse12pa6student/BSTTest.java
+++ ucsd-cse12-pa6/src/cse12pa6student/BSTTest.java
@@ -2,9 +2,102 @@
 
 package cse12pa6student;
 
-import org.junit.*;
+import static org.junit.Assert.*;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import org.junit.*;
 
 public class BSTTest {
 
+	@Test
+	public void testSetAndGet() {
+		BSTDefaultMap<String, Integer> b = new BSTDefaultMap<String, Integer>(100000);
+		assertEquals(0, b.size);
+		b.set("", 15);
+		assertEquals((Integer) 15, (Integer) b.get(""));
+		b.set("A", 20);
+		assertEquals((Integer) 100000, (Integer) b.get("C"));
+		assertEquals((Integer) 20, (Integer) b.get("A"));
+		b.set("A", 250);
+		assertEquals((Integer) 250, (Integer) b.get("A"));
+	}
+
+	@Test(expected = NoSuchElementException.class)
+	public void getNull() {
+		BSTDefaultMap<String, Integer> b = new BSTDefaultMap<String, Integer>(0);
+		b.set("A", null);
+		b.get("A");
+	}
+
+	@Test(expected = NoSuchElementException.class)
+	public void nullDefaultValue() {
+		BSTDefaultMap<String, Integer> b = new BSTDefaultMap<String, Integer>(null);
+		b.get("A");
+	}
+
+	@Test
+	public void testContainsKey() {
+		BSTDefaultMap<String, Integer> b = new BSTDefaultMap<String, Integer>(0);
+		assertFalse(b.containsKey("A"));
+		for (int i = 65; i < 91; i++) {
+			b.set((char) i + "", i);
+		}
+		assertTrue(b.containsKey("Z"));
+		assertFalse(b.containsKey("!@#$%^&(*)"));
+	}
+
+	@Test
+	public void testSize() {
+		BSTDefaultMap<String, Integer> b = new BSTDefaultMap<String, Integer>(0);
+		assertEquals(0, b.size());
+		for (int i = 65; i < 91; i++) {
+			b.set((char) i + "", i);
+		}
+		assertEquals(26, b.size());
+		b.set("a", 1);
+		b.set("Z", 26); // should not change the size
+		assertEquals(27, b.size());
+	}
+
+	@Test
+	public void testDefaultValue() {
+		BSTDefaultMap<String, Integer> b1 = new BSTDefaultMap<String, Integer>(null);
+		assertNull(b1.defaultValue());
+		BSTDefaultMap<String, Integer> b2 = new BSTDefaultMap<String, Integer>(0);
+		assertEquals((Integer) 0, b2.defaultValue());
+		BSTDefaultMap<String, Integer> b3 = new BSTDefaultMap<String, Integer>(1000000);
+		assertEquals((Integer) 1000000, b3.defaultValue());
+	}
+
+	@Test
+	public void testKeys() {
+		BSTDefaultMap<String, Integer> b = new BSTDefaultMap<String, Integer>(0);
+		List<String> lst = b.keys();
+		assertEquals(0, lst.size());
+		for (int i = 69; i >= 65; i--) {
+			b.set((char) i + "", i);
+		}
+		lst = b.keys();
+		assertEquals(5, lst.size());
+		for (int i = 0; i < 5; i++) {
+			assertEquals((char) (i + 65) + "", lst.get(i));
+		}
+	}
+
+	@Test
+	public void testValues() {
+		BSTDefaultMap<String, Integer> b = new BSTDefaultMap<String, Integer>(0);
+		List<Integer> lst = b.values();
+		assertEquals(0, lst.size());
+		for (int i = 69, j = 1; i >= 65; i--, j++) { //("E", 1), ("D", 2)...
+			b.set((char) i + "", j);
+		}
+		lst = b.values();
+		assertEquals(5, lst.size());
+		for (int i = 0; i < 5; i++) {
+			assertEquals((Integer) (5 - i), lst.get(i));
+		}
+	}
 }
